721ae0850ec3c1c895b883616c8102948fbb8859
Alessandro Panzieri <panzieri.ale@gmail.com>
2023-10-05 23:34:39 +0200

    commit

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 from flask import (
     Flask,
+    send_file,
     render_template
 )
 
 app = Flask(__name__)
 app.template_folder = "templates/min"
 
-app.route("/")
+@app.get("/")
 def index():
     return render_template("index.min.html")
+
+@app.route("/robots")
+def robots():
+    return send_file("robots.txt")
+
+@app.route("/sitemap")
+def sitemap():
+    return send_file("sitemap.txt")
 
 if __name__ == "__main__": app.run(debug = True)
