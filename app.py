90628af30d9aad959b2fcfd1bc675bab152d69f1
Alessandro Panzieri <panzieri.ale@gmail.com>
2023-10-06 00:36:52 +0200

    commit

--- app.py
+++ app.py
@@ -3,15 +3,57 @@ from flask import (
     send_file,
     render_template
 )
+from flask_socketio import (
+    emit,
+    SocketIO,
+    join_room,
+    leave_room
+)
 
 rooms = {}
+codes = set()
 
 app = Flask(__name__)
 app.template_folder = "templates/min"
+socketio = SocketIO(app)
 
 @app.get("/")
 def index():
     return render_template("index.min.html")
+
+@app.get("/<id>")
+def room(id):
+    socketio.emit("join", id)
+    return render_template("room.min.html")
+
+@socketio.on("join")
+def join(data):
+    print(data)
+    room_id = data["room_id"]
+    username = data["username"]
+    join_room(room_id)
+    if room_id not in rooms:
+        rooms[room_id] = []
+    rooms[room_id].append(username)
+    emit('update_players', rooms[room_id], room=room_id)
+    emit('message', f'{username} è entrato nella room.', room=room_id)
+
+@socketio.on('leave')
+def leave(data):
+    room_id = data['room_id']
+    username = data['username']
+    leave_room(room_id)
+    if room_id in rooms:
+        rooms[room_id].remove(username)
+        emit('update_players', rooms[room_id], room=room_id)
+        emit('message', f'{username} ha lasciato la room.', room=room_id)
+
+@socketio.on('chat_message')
+def chat_message(data):
+    room_id = data['room_id']
+    username = data['username']
+    message = data['message']
+    emit('chat_message', {'username': username, 'message': message}, room=room_id)
 
 @app.route("/robots")
 def robots():
@@ -21,4 +63,4 @@ def robots():
 def sitemap():
     return send_file("sitemap.txt")
 
-if __name__ == "__main__": app.run(debug = True)
+if __name__ == "__main__": socketio.run(app, debug = True, allow_unsafe_werkzeug = True)
