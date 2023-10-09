9e33a97108c857fa3066fb9248ac511fea9ea0a4
Alessandro Panzieri <panzieri.ale@gmail.com>
2023-10-10 00:52:14 +0200

    commit

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ socketio = SocketIO(app)
 def index(room = None):
     if room == None:
         room = str(uuid4())
-        rooms[room] = ["1B", "1C", "1D", "1S"]
+        rooms[room] = ["1B", "1C", "1D", "1S", "2B", "2C", "2D", "2S", "3B", "3C", "3D", "3S", "4B", "4C", "4D", "4S", "5B", "5C", "5D", "5S"]
         return render_template("index.min.html", room = room)
     elif room in list(rooms.keys()):
         return render_template("room.min.html", room = room)
@@ -33,8 +33,8 @@ def index(room = None):
 def join(data):
     join_room(data["room"])
 
-@socketio.on("draw_from_deck")
-def draw_from_deck(data):
+@socketio.on("pick_from_deck")
+def pick_from_deck(data):
     room = data["room"]
     deck = rooms[room]
 
@@ -43,9 +43,9 @@ def draw_from_deck(data):
     rooms[room] = deck
 
     if len(deck) == 0:
-        emit("stop_deck", room = room)
+        emit("void", room = room)
 
-    emit("draw", {"card": card}, room = data["user"])
+    emit("pick", {"card": card}, room = data["user"])
     
 @socketio.on("drop_from_deck")
 def drop_from_deck(data):
@@ -57,9 +57,19 @@ def drop_from_deck(data):
     rooms[room] = deck
 
     if len(deck) == 0:
-        emit("stop_deck", room = room)
+        emit("void", room = room)
         
     emit("drop", {"card": card}, room = room)
+
+@socketio.on("drop_from_hand")
+def drop_from_hand(data):
+    emit("drop", {"card": data["card"]}, room = data["room"])
+    
+@socketio.on("pick_from_drop")
+def pick_from_drop(data):
+    card = data["card"]
+    emit("pick", {"card": card}, room = data["user"])
+    emit("pull", {"card": card}, room = data["room"])
 
 @app.route("/robots")
 def robots():
