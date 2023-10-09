0d2048ce64027539984380fbd54171d8de2189be
Alessandro Panzieri <panzieri.ale@gmail.com>
2023-10-10 01:52:29 +0200

    commit

--- app.py
+++ app.py
@@ -22,7 +22,10 @@ socketio = SocketIO(app)
 def index(room = None):
     if room == None:
         room = str(uuid4())
-        rooms[room] = ["1B", "1C", "1D", "1S", "2B", "2C", "2D", "2S", "3B", "3C", "3D", "3S", "4B", "4C", "4D", "4S", "5B", "5C", "5D", "5S"]
+        rooms[room] = {
+            "drop": [],
+            "deck": ["1B", "1C", "1D", "1S", "2B", "2C", "2D", "2S", "3B", "3C", "3D", "3S", "4B", "4C", "4D", "4S", "5B", "5C", "5D", "5S"]
+        }
         return render_template("index.min.html", room = room)
     elif room in list(rooms.keys()):
         return render_template("room.min.html", room = room)
@@ -31,45 +34,65 @@ def index(room = None):
 
 @socketio.on("join")
 def join(data):
-    join_room(data["room"])
+    room = data["room"]
+
+    join_room(room)
+
+    emit("drop", {"cards": rooms[room]["drop"]}, room = data["user"])
 
 @socketio.on("pick_from_deck")
 def pick_from_deck(data):
     room = data["room"]
-    deck = rooms[room]
+    deck = rooms[room]["deck"]
 
     card = choice(deck)
-    deck.remove(card)
-    rooms[room] = deck
 
+    deck.remove(card)
+    rooms[room]["deck"] = deck
     if len(deck) == 0:
         emit("void", room = room)
 
     emit("pick", {"card": card}, room = data["user"])
-    
+
 @socketio.on("drop_from_deck")
 def drop_from_deck(data):
     room = data["room"]
-    deck = rooms[room]
-    
-    card = choice(deck)
-    deck.remove(card)
-    rooms[room] = deck
+    deck = rooms[room]["deck"]
+    drop = rooms[room]["drop"]
 
+    card = choice(deck)
+
+    deck.remove(card)
+    rooms[room]["deck"] = deck
     if len(deck) == 0:
         emit("void", room = room)
-        
-    emit("drop", {"card": card}, room = room)
+
+    drop.append(card)
+    rooms[room]["drop"] = drop
+
+    emit("drop", {"cards": rooms[room]["drop"]}, room = room)
 
 @socketio.on("drop_from_hand")
 def drop_from_hand(data):
-    emit("drop", {"card": data["card"]}, room = data["room"])
-    
+    room = data["room"]
+    drop = rooms[room]["drop"]
+
+    drop.append(data["card"])
+    rooms[room]["drop"] = drop
+
+    emit("drop", {"cards": rooms[room]["drop"]}, room = room)
+
 @socketio.on("pick_from_drop")
 def pick_from_drop(data):
+    room = data["room"]
     card = data["card"]
+    drop = rooms[room]["drop"] 
+
+    drop.remove(card)
+    rooms[room]["drop"] = drop
+
     emit("pick", {"card": card}, room = data["user"])
-    emit("pull", {"card": card}, room = data["room"])
+    emit("drop", {"cards": rooms[room]["drop"]}, room = room)
 
 @app.route("/robots")
 def robots():
