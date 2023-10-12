0fefb4afe27bf163a06733fd3e1dbabd50afa71e
Alessandro Panzieri <panzieri.ale@gmail.com>
2023-10-12 12:16:12 +0200

    commit

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ def sitemap():
 
 @app.route("/min/service-worker.min.js")
 def service_worker():
-    return send_file("/min/service-worker.min.js")
+    return send_file("min/service-worker.min.js")
 
 @app.errorhandler(404)
 @app.errorhandler(405)
