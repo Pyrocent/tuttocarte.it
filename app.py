2f5056926686f3e3eadbdb2a504c9ca4b8240922
Alessandro Panzieri <panzieri.ale@gmail.com>
2023-10-14 15:57:01 +0200

    commit

--- app.py
+++ app.py
@@ -12,6 +12,41 @@ from flask_socketio import (
     join_room
 )
 
+app = Flask(__name__)
+app.template_folder = "templates/min"
+socketio = SocketIO(app)
+
+@app.get("/")
+@app.get("/<room>")
+def index(room = None):
+    if room == None:
+        room = str(uuid4())
+        return render_template("index.min.html", room = room, show = True)
+    return render_template("index.min.html", room = room)
+
+@app.route("/robots.txt")
+def robots():
+    return send_file("robots.txt")
+
+@app.route("/sitemap.xml")
+def sitemap():
+    return send_file("sitemap.xml")
+
+@app.route("/manifest.json")
+def manifest():
+    return send_file("manifest.json")
+
+@app.route("/service-worker.js")
+def service_worker():
+    return send_file("service-worker.js")
+
+@app.errorhandler(404)
+@app.errorhandler(405)
+def error(_):
+    return redirect("/")
+
+if __name__ == "__main__": socketio.run(app)
+
 # "deck": ["1B", "1C", "1D", "1S", "2B", "2C", "2D", "2S", "3B", "3C", "3D", "3S", "4B", "4C", "4D", "4S", "5B", "5C", "5D", "5S"]
 
 # @socketio.on("join")
@@ -79,38 +114,3 @@ from flask_socketio import (
 
 #     emit("pick", {"card": card}, room = data["user"])
 #     emit("drop", {"cards": rooms[room]["drop"]}, room = room)
-
-app = Flask(__name__)
-app.template_folder = "templates/min"
-socketio = SocketIO(app)
-
-@app.get("/")
-@app.get("/<room>")
-def index(room = None):
-    if room == None:
-        room = str(uuid4())
-        return render_template("index.min.html", room = room, show = True)
-    return render_template("index.min.html", room = room)
-
-@app.route("/robots.txt")
-def robots():
-    return send_file("robots.txt")
-
-@app.route("/sitemap.xml")
-def sitemap():
-    return send_file("sitemap.xml")
-
-@app.route("/manifest.json")
-def manifest():
-    return send_file("manifest.json")
-
-@app.route("/service-worker.js")
-def service_worker():
-    return send_file("service-worker.js")
-
-@app.errorhandler(404)
-@app.errorhandler(405)
-def error(_):
-    return redirect("/")
-
-if __name__ == "__main__": socketio.run(app, debug = True, allow_unsafe_werkzeug = True)
