6ef757596a5b9658d77ca4640fa004d684915a14
Alessandro Panzieri <panzieri.ale@gmail.com>
2023-10-17 12:58:52 +0200

    commit

--- app.py
+++ app.py
@@ -12,12 +12,12 @@ from flask_socketio import (
     join_room
 )
 
+it = ["1B", "1C", "1D", "1S", "2B", "2C", "2D", "2S", "3B", "3C", "3D", "3S", "4B", "4C", "4D", "4S", "5B", "5C", "5D", "5S", "6B", "6C", "6D", "6S", "7B", "7C", "7D", "7S", "8B", "8C", "8D", "8S", "9B", "9C", "9D", "9S", "10B", "10C", "10D", "10S"]
+fr = ["1C", "1F", "1P", "1Q", "2C", "2F", "2P", "2Q", "3C", "3F", "3P", "3Q", "4C", "4F", "4P", "4Q", "5C", "5F", "5P", "5Q", "6C", "6F", "6P", "6Q", "7C", "7F", "7P", "7Q", "8C", "8F", "8P", "8Q", "9C", "9F", "9P", "9Q", "10C", "10F", "10P", "10Q", "JC", "JF", "JP", "JQ", "QC", "QF", "QP", "QQ", "KC", "KF", "KP", "KQ", "BJ", "RJ"]
+
 app = Flask(__name__)
 app.template_folder = "templates/min"
 socketio = SocketIO(app)
-
-it = ["1B", "1C", "1D", "1S", "2B", "2C", "2D", "2S", "3B", "3C", "3D", "3S", "4B", "4C", "4D", "4S", "5B", "5C", "5D", "5S", "6B", "6C", "6D", "6S", "7B", "7C", "7D", "7S", "8B", "8C", "8D", "8S", "9B", "9C", "9D", "9S", "10B", "10C", "10D", "10S"]
-fr = ["1C", "1F", "1P", "1Q", "2C", "2F", "2P", "2Q", "3C", "3F", "3P", "3Q", "4C", "4F", "4P", "4Q", "5C", "5F", "5P", "5Q", "6C", "6F", "6P", "6Q", "7C", "7F", "7P", "7Q", "8C", "8F", "8P", "8Q", "9C", "9F", "9P", "9Q", "10C", "10F", "10P", "10Q", "JC", "JF", "JP", "JQ", "QC", "QF", "QP", "QQ", "KC", "KF", "KP", "KQ", "BJ", "RJ"]
 
 @app.get("/")
 @app.get("/<room>")
@@ -30,7 +30,7 @@ def index(room = None):
 @socketio.on("join")
 def join(data):
     join_room(data["room"])
-    
+
 @socketio.on("table")
 def table(data):
     emit("table", {"table": data["table"]}, room = data["room"])
@@ -43,13 +43,13 @@ def robots():
 def sitemap():
     return send_file("sitemap.xml")
 
-@app.route("/manifest.json")
-def manifest():
-    return send_file("manifest.json")
+# @app.route("/manifest.json")
+# def manifest():
+#     return send_file("manifest.json")
 
-@app.route("/service-worker.js")
-def service_worker():
-    return send_file("service-worker.js")
+# @app.route("/service-worker.js")
+# def service_worker():
+#     return send_file("service-worker.js")
 
 @app.errorhandler(404)
 @app.errorhandler(405)
