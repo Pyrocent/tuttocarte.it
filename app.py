99516e5fd31be2396886ac5ef24182ebbcb66cc3
Alessandro Panzieri <panzieri.ale@gmail.com>
2023-10-17 22:33:40 +0200

    commit

--- app.py
+++ app.py
@@ -17,9 +17,9 @@ it = ["1B", "1C", "1D", "1S", "2B", "2C", "2D", "2S", "3B", "3C", "3D", "3S", "4
 fr = ["1C", "1F", "1P", "1Q", "2C", "2F", "2P", "2Q", "3C", "3F", "3P", "3Q", "4C", "4F", "4P", "4Q", "5C", "5F", "5P", "5Q", "6C", "6F", "6P", "6Q", "7C", "7F", "7P", "7Q", "8C", "8F", "8P", "8Q", "9C", "9F", "9P", "9Q", "10C", "10F", "10P", "10Q", "JC", "JF", "JP", "JQ", "QC", "QF", "QP", "QQ", "KC", "KF", "KP", "KQ", "BJ", "RJ"]
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 app.secret_key = token_hex(16)
 app.template_folder = "templates/min"
-socketio = SocketIO(app)
 
 @app.get("/")
 @app.get("/<room>")
@@ -45,17 +45,9 @@ def robots():
 def sitemap():
     return send_file("sitemap.xml")
 
-# @app.route("/manifest.json")
-# def manifest():
-#     return send_file("manifest.json")
-
-# @app.route("/service-worker.js")
-# def service_worker():
-#     return send_file("service-worker.js")
-
 @app.errorhandler(404)
 @app.errorhandler(405)
 def error(_):
     return redirect("/")
 
-if __name__ == "__main__": socketio.run(app, debug = True)
+if __name__ == "__main__": socketio.run(app, debug = True, allow_unsafe_werkzeug = True)
