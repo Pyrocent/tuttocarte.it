41ea14a7502af734087a540324c211d817364ff7
Alessandro Panzieri <panzieri.ale@gmail.com>
2023-10-29 02:04:58 +0100

    commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from cryptography.fernet import Fernet
 from secrets import token_hex
-from random import sample
+from random import shuffle
 from time import time
 from flask import (
     Flask,
@@ -14,51 +14,59 @@ from flask_socketio import (
     join_room
 )
 
-it_deck = ["1B", "1C", "1D", "1S", "2B", "2C", "2D", "2S", "3B", "3C", "3D", "3S", "4B", "4C", "4D", "4S", "5B", "5C", "5D", "5S", "6B", "6C", "6D", "6S", "7B", "7C", "7D", "7S", "8B", "8C", "8D", "8S", "9B", "9C", "9D", "9S", "10B", "10C", "10D", "10S"]
-fr_deck = ["1C", "1F", "1P", "1Q", "2C", "2F", "2P", "2Q", "3C", "3F", "3P", "3Q", "4C", "4F", "4P", "4Q", "5C", "5F", "5P", "5Q", "6C", "6F", "6P", "6Q", "7C", "7F", "7P", "7Q", "8C", "8F", "8P", "8Q", "9C", "9F", "9P", "9Q", "10C", "10F", "10P", "10Q", "JC", "JF", "JP", "JQ", "QC", "QF", "QP", "QQ", "KC", "KF", "KP", "KQ", "BJ", "RJ"]
+encryption_key = Fernet.generate_key()
+encrypted_italian_deck = [Fernet(encryption_key).encrypt(card.encode()).decode() for card in ["1B", "1C", "1D", "1S", "2B", "2C", "2D", "2S", "3B", "3C", "3D", "3S", "4B", "4C", "4D", "4S", "5B", "5C", "5D", "5S", "6B", "6C", "6D", "6S", "7B", "7C", "7D", "7S", "8B", "8C", "8D", "8S", "9B", "9C", "9D", "9S", "10B", "10C", "10D", "10S"]]
+encrypted_french1_deck = [Fernet(encryption_key).encrypt(card.encode()).decode() for card in ["1C", "1F", "1P", "1Q", "2C", "2F", "2P", "2Q", "3C", "3F", "3P", "3Q", "4C", "4F", "4P", "4Q", "5C", "5F", "5P", "5Q", "6C", "6F", "6P", "6Q", "7C", "7F", "7P", "7Q", "8C", "8F", "8P", "8Q", "9C", "9F", "9P", "9Q", "10C", "10F", "10P", "10Q", "JC", "JF", "JP", "JQ", "QC", "QF", "QP", "QQ", "KC", "KF", "KP", "KQ", "BJ", "RJ"]]
+encrypted_french2_deck = [Fernet(encryption_key).encrypt(card.encode()).decode() for card in ["1C", "1F", "1P", "1Q", "2C", "2F", "2P", "2Q", "3C", "3F", "3P", "3Q", "4C", "4F", "4P", "4Q", "5C", "5F", "5P", "5Q", "6C", "6F", "6P", "6Q", "7C", "7F", "7P", "7Q", "8C", "8F", "8P", "8Q", "9C", "9F", "9P", "9Q", "10C", "10F", "10P", "10Q", "JC", "JF", "JP", "JQ", "QC", "QF", "QP", "QQ", "KC", "KF", "KP", "KQ", "BJ", "RJ"]]
 
 app = Flask(__name__)
 app.secret_key = token_hex(16)
 app.template_folder = "templates/min"
-socketio = SocketIO(app)
+socket = SocketIO(app)
 
 @app.get("/")
 @app.get("/<room>")
 def index(room = None):
     if room is not None:
         return render_template("index.min.html", room = room)
-    return render_template("index.min.html", room = int(time()), dealer = True)
 
-@socketio.on("join")
+    shuffle(encrypted_italian_deck)
+    shuffle(encrypted_french1_deck)
+    shuffle(encrypted_french2_deck)
+
+    return render_template(
+        "index.min.html",
+        room = int(time()),
+        dealer = True,
+        html = f"""
+            <div class = "deck">
+                {"".join([f'<img id = "{card}" class = "italian card" src = "static/assets/decks/italian/BACK.jpg" alt = "card">' for card in encrypted_italian_deck])}
+            </div>
+            <div class = "deck">
+                {"".join([f'<img id = "{card}" class = "french1 card" src = "static/assets/decks/french1/BACK.jpg" alt = "card">' for card in encrypted_french1_deck])}
+            </div>
+            <div class = "deck">
+                {"".join([f'<img id = "{card}" class = "french2 card" src = "static/assets/decks/french2/BACK.jpg" alt = "card">' for card in encrypted_french2_deck])}
+            </div>
+            <img id = "fiche1" class = "fiche" src = "static/assets/fiches/FICHE1.png" alt = "fiche">
+            <img id = "fiche2" class = "fiche" src = "static/assets/fiches/FICHE2.png" alt = "fiche">
+            <img id = "fiche3" class = "fiche" src = "static/assets/fiches/FICHE3.png" alt = "fiche">
+            <img id = "dealer" class = "fiche" src = "static/assets/fiches/DEALER.png" alt = "fiche">
+        """)
+
+@socket.on("join")
 def join(data):
     join_room(data["room"])
 
-@socketio.on("start")
-def start(data):
-    key = Fernet.generate_key()
-    cipher_suite = Fernet(key)
-    html = f"""
-        <div class = "deck">
-            {"".join([f"<img id = '{cipher_suite.encrypt(card.encode()).decode()}' class = 'it card' src = 'static/assets/decks/it/BACK.jpg' alt = 'card'>" for card in sample(it_deck, 40)])}
-        </div>
-        <div class = "deck">
-            {"".join([f"<img id = '{cipher_suite.encrypt(card.encode()).decode()}' class = 'fr-b card' src = 'static/assets/decks/fr-b/BACK.jpg' alt = 'card'>" for card in sample(fr_deck, 54)])}
-        </div>
-        <div class = "deck">
-            {"".join([f"<img id = '{cipher_suite.encrypt(card.encode()).decode()}' class = 'fr-r card' src = 'static/assets/decks/fr-r/BACK.jpg' alt = 'card'>" for card in sample(fr_deck, 54)])}
-        </div>
-        <img id = 'fiche-1' class = 'fiche' src = 'static/assets/fiches/FICHE-1.png' alt = 'fiche'>
-        <img id = 'fiche-2' class = 'fiche' src = 'static/assets/fiches/FICHE-2.png' alt = 'fiche'>
-        <img id = 'fiche-3' class = 'fiche' src = 'static/assets/fiches/FICHE-3.png' alt = 'fiche'>
-        <img id = 'dealer' class = 'fiche' src = 'static/assets/fiches/DEALER.png' alt = 'fiche'>
-    """
-    emit("table", {"html": html, "key": key.decode()}, room = data["room"])
+@socket.on("draw")
+def draw(data):
+    emit("draw", {"card": Fernet(encryption_key).decrypt(data["id"]).decode()}, room = data["room"])
 
-@socketio.on("table")
+@socket.on("table")
 def table(data):
     emit("table", {"html": data["html"]}, room = data["room"])
 
-@socketio.on("notes")
+@socket.on("notes")
 def notes(data):
     emit("notes", {"notes": data["notes"]}, room = data["room"])
 
@@ -83,4 +91,4 @@ def service_worker():
 def error(_):
     return redirect("/")
 
-if __name__ == "__main__": socketio.run(app, debug = True, allow_unsafe_werkzeug = True)
+if __name__ == "__main__": socket.run(app,allow_unsafe_werkzeug=True)
