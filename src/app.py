05a3b0ed4677839bc4bb3947a532fae93ff22097
alessandro <panzieri.ale@gmail.com>
2024-06-11 01:43:49 +0200

    commit

--- src/app.py
+++ src/app.py
@@ -1,14 +1,19 @@
 from os import listdir
-from random import sample
+from random import choice
 from secrets import token_hex
 from flask_socketio import emit, SocketIO, join_room
 from flask import Flask, request, redirect, send_file, render_template
+
+decks = {
+    "it": listdir("./src/static/assets/decks/it"),
+    "fr": listdir("./src/static/assets/decks/fr")
+}
 
 socketio = SocketIO(app := Flask(__name__))
 app.secret_key = token_hex(16)
 
 @app.get("/")
-def index(): return render_template("index.html", ita_deck = sample(listdir("./src/static/assets/decks/ita"), 40), fr1_deck = sample(listdir(f"./src/static/assets/decks/fra"), 54), fr2_deck = sample(listdir(f"./src/static/assets/decks/fra"), 54))
+def index(): return render_template("index.html")
 
 @app.get("/robots.txt")
 @app.get("/sitemap.xml")
@@ -25,7 +30,15 @@ def error(_): return redirect("/")
 def handle_join(data): join_room(data["room"]), emit("join", {"user": request.sid}, to = data["room"], include_self = False)
 
 @socketio.on("play")
-def handle_play(data): emit("play", {"html": data["html"]}, to = data["user"] if not data["room"] else data["room"], include_self = False)
+def handle_play(data): emit("play", {"html": data["html"]}, to = data["user"])
+
+@socketio.on("show")
+def handle_show(data): emit("show", {"card": data["card"], "deck": data["deck"], "exit": data["exit"] if not isinstance(data["exit"], list) else choice([i for i in decks[data["deck"]] if i not in data["exit"]])}, to = data["room"])
+
+@socketio.on("hide")
+def handle_hide(data): emit("hide", {"card": data["card"], "deck": data["deck"]}, to = data["room"])
 
 @socketio.on("hand")
-def handle_hand(data): emit("hand", {"html": data["html"], "position": {"x": data["position"]["x"], "y": data["position"]["y"], "z": data["position"]["z"]}}, to = data["room"], include_self = False)
+def handle_hand(data): emit("hand", {"html": data["html"], "x": data["x"], "y": data["y"], "z": data["z"]}, to = data["room"], include_self = False)
+
+socketio.run(app, debug = True)
--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,5 +1,5 @@
 $(() => {
-    let room, socketio;
+    let exit = {};
     fetch("https://api.ipify.org?format=json")
         .then(response => response.json())
         .then(data => { room = data.ip; })
@@ -7,46 +7,49 @@ $(() => {
             socketio = io();
             socketio.on("connect", function () {
                 socketio.emit("join", { room });
-                socketio.on("join", function (data) { socketio.emit("play", { room: false, user: data.user, html: $("#table").html() }); });
+                socketio.on("join", function (data) { socketio.emit("play", { user: data.user, html: $("#table").html() }); });
                 socketio.on("play", function (data) { $("#table").html(data.html); });
+                socketio.on("show", function (data) {
+                    exit[data.card] = data.exit;
+                    $(`#table img:nth-child(${data.card + 1})`).attr("src", `../static/assets/decks/${data.deck}/${data.exit}`);
+                });
+                socketio.on("hide", function (data) {
+                    $(`#table img:nth-child(${data.card + 1})`).attr("src", `../static/assets/decks/backs/${data.deck}.png`);
+                });
                 socketio.on("hand", function (data) {
-                    $("#table").html(data.html + `<img id = "hand-icon" src = "static/assets/other/hand.png" height = "50px" style = "position: absolute; left: ${data.position.x}; top: ${data.position.y}; z-index: ${data.position.z}" alt = "hand-icon">`);
+                    $("#table").html(data.html + `<img id = "hand-icon" src = "static/assets/other/hand.png" height = "50px" style = "position: absolute; left: ${data.x}; top: ${data.y}; z-index: ${data.z}" alt = "hand-icon">`);
                     $("#hand-icon").fadeOut(1500, function () {
                         $(this).remove();
                     });
                 });
             });
 
-            function turn(target) {
-                $(target).attr(
-                    "src",
-                    $(target).attr("src").startsWith("../static/assets/decks/backs/")
-                        ? `../static/assets/decks/${target.classList[0]}/${target.id}`
-                        : `../static/assets/decks/backs/${target.classList[0]}/${target.classList[1]}.png`
-                );
-            }
+            $("#table .card").draggable({});
 
             interact("#table .card")
                 .on("tap", (event) => {
-                    turn(event.target);
-                    socketio.emit("play", { room, user: false, html: $("#table").html() });
+                    card = $(event.target).index();
+                    deck = event.target.classList[0];
+                    $(event.target).attr("src").startsWith("../static/assets/decks/backs/")
+                        ? socketio.emit("show", { room, card: card, deck: deck.replace(/\d+/g, ""), exit: exit[card] ?? Object.values(exit) })
+                        : socketio.emit("hide", { room, card: card, deck: deck })
                 })
                 .on("hold", (event) => {
-                    var x = $(event.target).css("left");
-                    var y = $(event.target).css("top");
-                    var z = $(event.target).css("z-index");
-                    $("#hand").prepend($(event.target).removeAttr("style"));
-                    socketio.emit("hand", { room, html: $("#table").html(), position: { x: x, y: y, z: z } });
+                    x = $(event.target).css("left");
+                    y = $(event.target).css("top");
+                    z = $(event.target).css("z-index");
+                    $("#hand div").prepend($(event.target).removeAttr("style"));
+                    socketio.emit("hand", { room, html: $("#table").html(), x: x, y: y, z: z });
                 });
 
-            interact("#hand .card")
-                .on("tap", (event) => {
-                    turn(event.target);
-                })
-                .on("hold", (event) => {
-                    $("#table").prepend($(event.target).removeAttr("style").css({
-                        "transform": `translate3d(${(($("body").width() / 2) - 34.325 + parseFloat($(event.target).css("left")))}px, ${$("#table").height() - $(event.target).height()}px, 0)`
-                    }));
-                });
+            // interact("#hand .card")
+            //     .on("tap", (event) => {
+            //         turn(event.target);
+            //     })
+            //     .on("hold", (event) => {
+            //         $("#table").prepend($(event.target).removeAttr("style").css({
+            //             "transform": `translate3d(${(($("body").width() / 2) - 34.325 + parseFloat($(event.target).css("left")))}px, ${$("#table").height() - $(event.target).height()}px, 0)`
+            //         }));
+            //     });
         });
 });
